7a1118ad5d43d1ce891acff9248e35e564a7e2cd
Luiz <luizcunha3@gmail.com>
2025-01-01 15:49:00 -0300

    Initial Commit

---
+++ InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by Luiz Cunha on 01/01/25.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

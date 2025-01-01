42d35106be811bde33d78a8f96555a32b6ed0e80
Luiz <luizcunha3@gmail.com>
2025-01-01 16:00:36 -0300

    Adiciona imagem do theo

---
+++ InstaClone/PostHeaderView.swift
@@ -0,0 +1,19 @@
+//
+//  PostHeaderView.swift
+//  InstaClone
+//
+//  Created by Luiz Cunha on 01/01/25.
+//
+
+import SwiftUI
+
+struct PostHeaderView: View {
+    var body: some View {
+        Image("profileTheo")
+        
+    }
+}
+
+#Preview {
+    PostHeaderView()
+}
